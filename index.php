d5fd5fddc8588b120b5c476c93bba5f157ca35ef
Iris <iris.jansen@student.aventus.nl>
2018-11-15 11:15:37 +0100

    map images?

--- index.php
+++ index.php
@@ -7,19 +7,19 @@ include 'includes/header.php';
             <p>To make the tabs toggleable, add the data-toggle="tab" attribute to each link. Then add a .tab-pane class with a unique ID for every tab and wrap them inside a div element with class .tab-content.</p>
 
             <ul class="nav nav-tabs nav-stacked">
-                <li class="active"><a data-toggle="tab" href="#home">Home</a></li>
-                <li><a data-toggle="tab" href="#menu1">Menu 1</a></li>
-                <li><a data-toggle="tab" href="#menu2">Menu 2</a></li>
-                <li><a data-toggle="tab" href="#menu3">Menu 3</a></li>
+                <li class="active"><a data-toggle="tab" href="#menu1">Webapplicaties</a></li>
+                <li><a data-toggle="tab" href="#menu2">Desktopapplicaties</a></li>
+                <li><a data-toggle="tab" href="#menu3">Games</a></li>
             </ul>
-
+        </div>
+        <div class="col-sm-9">
             <div class="tab-content">
                 <div id="home" class="tab-pane fade in active">
-                    <h3>HOME</h3>
+                    <h3>Webapplicaties</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                 </div>
                 <div id="menu1" class="tab-pane fade">
-                    <h3>Menu 1</h3>
+                    <h3>Desktopapplicaties</h3>
                     <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                 </div>
                 <div id="menu2" class="tab-pane fade">
@@ -31,9 +31,6 @@ include 'includes/header.php';
                     <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
                 </div>
             </div>
-        </div>
-        <div class="col-sm-9">
-
         </div>
     </div>
 
