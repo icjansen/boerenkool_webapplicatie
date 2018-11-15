67a866e9465e0fe2ce0e91846920548dc8028efa
Iris <iris.jansen@student.aventus.nl>
2018-11-15 12:00:20 +0100

    .iml

--- includes/header.php
+++ includes/header.php
@@ -23,15 +23,10 @@
             <a class="navbar-brand" href="#">WebSiteName</a>
         </div>
         <div class="collapse navbar-collapse" id="myNavbar">
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#">Page 1</a></li>
-                <li><a href="#">Page 2</a></li>
-                <li><a href="#">Page 3</a></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-                <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+            <ul class="nav nav-tabs nav-stacked">
+                <li class="active"><a data-toggle="tab" href="#home">Webapplicaties</a></li>
+                <li><a data-toggle="tab" href="#menu1">Desktopapplicaties</a></li>
+                <li><a data-toggle="tab" href="#menu2">Games</a></li>
             </ul>
         </div>
     </div>
