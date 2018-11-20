fa161f7e74ea1ca74ff810734cd609a14d4041fa
Iris <iris.jansen@student.aventus.nl>
2018-11-20 16:30:51 +0100

    alles uit database

--- index.php
+++ index.php
@@ -58,14 +58,9 @@ include 'Classes/ProjectDetails.php';
                                                     $i = 0;
                                                     while($row2=mysqli_fetch_array($result2)) {
                                                         $i++;
-                                                        var_dump($row2[0]);
                                                         ?>
-
-
-
-
                                                         <div class="item<?php if($i == 1) { echo " active"; } ?>">
-                                                            <img src="./images/placeholder.png" alt="Los Angeles" style="width:100%;">
+                                                            <img src="<?php echo $row2['image'] ?>" alt="project_images" style="width:100%;">
                                                         </div>
 
                                                         <?php
@@ -90,11 +85,11 @@ include 'Classes/ProjectDetails.php';
 
                                         </div>
                                         <div class="text_container col-xs-12 col-sm-6">
-                                            ergewgfrehgrefgrt
+                                            <?php echo $row['project_description'] ?>
                                         </div>
                                     </div>
                                     <div class="modal-footer">
-                                        <a href="" class="btn btn-success visit_website">Bezoek website</a>
+                                        <a href="<?php echo $row['download_link'] ?>" class="btn btn-success visit_website">Bezoek website</a>
                                         <button type="button" class="btn btn-danger" data-dismiss="modal">Sluiten
                                         </button>
                                     </div>
@@ -113,6 +108,7 @@ include 'Classes/ProjectDetails.php';
                     $project = new Project();
                     $result = $project->getProjects("desktop");
                     while($row=mysqli_fetch_array($result)) {
+                        $project_id = $row['project_id'];
                         ?>
                         <div class="custom_card col-xs-12 col-sm-4">
                             <div class="card_img">
@@ -123,9 +119,70 @@ include 'Classes/ProjectDetails.php';
                             <div class="card_text">
                                 <p><?php echo $row['project_description'] ?></p>
                             </div>
-                            <a class="card_btn btn btn-primary" href="#">Ga naar project</a>
+                            <button type="button" class="btn btn-info btn-lg card_btn btn btn-primary" data-toggle="modal" data-target="#myModal<?php echo $row['project_id'] ?>">Ga naar project</button>
+                        </div>
+
+                        <div class="modal fade" id="myModal<?php echo $row['project_id'] ?>" role="dialog">
+                            <div class="modal-dialog modal-lg">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                        <h4 class="modal-title"><?php echo $row['project_name'] . ", gemaakt door " . $row['student_name'] ?></h4>
+                                    </div>
+                                    <div class="modal-body project_details">
+                                        <div class="img_container col-xs-12 col-sm-6">
+
+                                            <h2>Carousel Example</h2>
+                                            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+                                                <div class="carousel-inner">
+
+                                                    <?php
+                                                    $project2 = new ProjectDetails();
+                                                    $result2 = $project2->getProjectDetails($project_id);
+                                                    $i = 0;
+                                                    while($row2=mysqli_fetch_array($result2)) {
+                                                        $i++;
+                                                        ?>
+                                                        <div class="item<?php if($i == 1) { echo " active"; } ?>">
+                                                            <img src="<?php echo $row2['image'] ?>" alt="project_images" style="width:100%;">
+                                                        </div>
+
+                                                        <?php
+                                                    } ?>
+
+
+
+                                                    <!-- Left and right controls -->
+                                                    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                                                        <span class="glyphicon glyphicon-chevron-left"></span>
+                                                        <span class="sr-only">Previous</span>
+                                                    </a>
+                                                    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                                                        <span class="glyphicon glyphicon-chevron-right"></span>
+                                                        <span class="sr-only">Next</span>
+                                                    </a>
+                                                </div>
+                                            </div>
+
+
+
+
+                                        </div>
+                                        <div class="text_container col-xs-12 col-sm-6">
+                                            <?php echo $row['project_description'] ?>
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <a href="" class="btn btn-success visit_website" download>Download applicatie</a>
+                                        <button type="button" class="btn btn-danger" data-dismiss="modal">Sluiten
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         <?php
+
                     }
                     ?>
                 </div>
@@ -135,19 +192,81 @@ include 'Classes/ProjectDetails.php';
                     $project = new Project();
                     $result = $project->getProjects("game");
                     while($row=mysqli_fetch_array($result)) {
+                        $project_id = $row['project_id'];
                         ?>
-                        <div class="custom_card col-xs-12 col-sm-4">
-                            <div class="card_img">
-                                <img src="<?php echo $row['project_thumbnail'] ?>" alt="project_thumbnail"
-                                     style="width: 100%; height: 100%;">
-                            </div>
-                            <h4 class="card_title"><?php echo $row['project_name'] . ", gemaakt door " . $row['student_name'] ?></h4>
-                            <div class="card_text">
-                                <p><?php echo $row['project_description'] ?></p>
-                            </div>
-                            <a class="card_btn btn btn-primary" href="#">Ga naar project</a>
+                    <div class="custom_card col-xs-12 col-sm-4">
+                        <div class="card_img">
+                            <img src="<?php echo $row['project_thumbnail'] ?>" alt="project_thumbnail"
+                                 style="width: 100%; height: 100%;">
                         </div>
-                        <?php
+                        <h4 class="card_title"><?php echo $row['project_name'] . ", gemaakt door " . $row['student_name'] ?></h4>
+                        <div class="card_text">
+                            <p><?php echo $row['project_description'] ?></p>
+                        </div>
+                        <button type="button" class="btn btn-info btn-lg card_btn btn btn-primary" data-toggle="modal" data-target="#myModal<?php echo $row['project_id'] ?>">Ga naar project</button>
+                    </div>
+
+                    <div class="modal fade" id="myModal<?php echo $row['project_id'] ?>" role="dialog">
+                        <div class="modal-dialog modal-lg">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                    <h4 class="modal-title"><?php echo $row['project_name'] . ", gemaakt door " . $row['student_name'] ?></h4>
+                                </div>
+                                <div class="modal-body project_details">
+                                    <div class="img_container col-xs-12 col-sm-6">
+
+                                        <h2>Carousel Example</h2>
+                                        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+                                            <div class="carousel-inner">
+
+                                                <?php
+                                                $project2 = new ProjectDetails();
+                                                $result2 = $project2->getProjectDetails($project_id);
+                                                $i = 0;
+                                                while($row2=mysqli_fetch_array($result2)) {
+                                                    $i++;
+                                                    ?>
+                                                    <div class="item<?php if($i == 1) { echo " active"; } ?>">
+                                                        <img src="<?php echo $row2['image'] ?>" alt="project_images" style="width:100%;">
+                                                    </div>
+
+                                                    <?php
+                                                } ?>
+
+
+
+                                                <!-- Left and right controls -->
+                                                <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                                                    <span class="glyphicon glyphicon-chevron-left"></span>
+                                                    <span class="sr-only">Previous</span>
+                                                </a>
+                                                <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                                                    <span class="glyphicon glyphicon-chevron-right"></span>
+                                                    <span class="sr-only">Next</span>
+                                                </a>
+                                            </div>
+                                        </div>
+
+
+
+
+                                    </div>
+                                    <div class="text_container col-xs-12 col-sm-6">
+                                        <?php echo $row['project_description'] ?>
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <a href="" class="btn btn-success visit_website" download>Download applicatie>Bezoek website</a>
+                                    <button type="button" class="btn btn-danger" data-dismiss="modal">Sluiten
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <?php
+
                     }
                     ?>
                 </div>
